c0595901d1a3e7d7c1f57fd7ccfaf883c0b6e7c4
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-25 15:22:42 +0200

    sample generator

--- app/src/main/java/ru/nobird/junction/ui/view/PlotView.kt
+++ app/src/main/java/ru/nobird/junction/ui/view/PlotView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import ru.nobird.junction.data.PlotBuffer
 
@@ -14,13 +15,7 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
     private var baselineY = 0f
     private var mult = 1f
 
-    val data = PlotBuffer(10000)
-
-    init {
-        for (i in 0..10000) {
-            data.add(Math.abs(Math.cos(i.toDouble() / 250)).toFloat())
-        }
-    }
+    val data = PlotBuffer(500)
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = 0xFF58D088.toInt()
@@ -28,9 +23,9 @@ constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 
     override fun onDraw(canvas: Canvas) {
         val dx = width.toFloat() / data.size
-        mult = height.toFloat()
+        mult = height.toFloat() / 2
         for (i in 1 .. data.size) {
-            canvas.drawLine(dx * (i - 1), mult - data[i - 1] * mult, dx * i, mult - data[i] * mult, paint)
+            canvas.drawLine(dx * (i - 1), data[i - 1] * mult, dx * i, data[i] * mult, paint)
         }
     }
 
