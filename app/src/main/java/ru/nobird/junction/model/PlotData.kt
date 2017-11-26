ea9d35990f2248fbfde7e30ca3f36ada3e2176eb
Igor.Ilyin <Igor.Ilyin@jetbrains.com>
2017-11-25 16:36:57 -0800

    PlotData and generator

---
+++ app/src/main/java/ru/nobird/junction/model/PlotData.kt
@@ -0,0 +1,10 @@
+package ru.nobird.junction.model
+
+/**
+ * Created by Owntage on 11/25/2017.
+ *
+ * @param quality 0..1 value, representing the quality of performing.
+ * The bigger - the better.
+ */
+
+class PlotData(val actualMagnitude: Double, val idealMagnitude: Double, val quality: Double)
---
+++ app/src/main/java/ru/nobird/junction/processing/PlotGenerator.java
@@ -0,0 +1,9 @@
+package ru.nobird.junction.processing;
+
+/**
+ * Created by Owntage on 11/25/2017.
+ */
+
+public class PlotGenerator {
+
+}
